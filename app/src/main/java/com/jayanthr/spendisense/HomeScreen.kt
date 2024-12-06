e287a56440370bbaed8bf505011a8447733953bb
El Jay <jayanthrahul3@gmail.com>
2024-12-06 19:10:48 +0530

    v0.1.1.01

--- app/src/main/java/com/jayanthr/spendisense/HomeScreen.kt
+++ app/src/main/java/com/jayanthr/spendisense/HomeScreen.kt
@@ -5,15 +5,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
@@ -26,6 +27,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.jayanthr.spendisense.ui.theme.zinc
+import androidx.compose.ui.Alignment
+import androidx.constraintlayout.compose.Dimension
 
 
 @Composable
@@ -56,7 +59,7 @@ fun HomeScreen(){
                     Text(text = "Jayanth", fontSize = 20.sp, fontWeight = FontWeight.Bold , color = Color.White)
                 }
                 Image(
-                    painter = painterResource(id = R.drawable.dotsmenu_homepage),
+                    painter = painterResource(id = R.drawable.ic_notifications),
                     contentDescription = null,
                     modifier = Modifier.align(Alignment.CenterEnd)
 
@@ -68,6 +71,17 @@ fun HomeScreen(){
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                 })
+
+            TransactionList(modifier = Modifier.fillMaxSize().constrainAs(list){
+              top.linkTo(card.bottom)
+              start.linkTo(parent.start)
+                bottom.linkTo(parent.bottom)
+              end.linkTo(parent.end)
+                height = Dimension.fillToConstraints
+
+            })
+
+
         }
     }
 }
@@ -83,19 +97,115 @@ fun CardItem( modifier: Modifier){
         .background(zinc)
         .padding(16.dp)
 
-        ) {
-            Row {
-                Column {
-                    Text(text = "Total Balance", fontSize = 16.sp, color = Color.White)
-                    Text(
-                        text = "INR 25,000",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
-                    )
-                }
+    ) {
+        Box( modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)) {
+            Column (modifier = Modifier.align(Alignment.CenterStart)) {
+                Text(text = "Total's Spending", fontSize = 16.sp, color = Color.White)
+                Text(
+                    text = "₹ 25",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                )
             }
+            Image( painter = painterResource(id = R.drawable.dotsmenu_homepage),
+                contentDescription = null,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+        }
+
+        Box (modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f),
+            ){
+            CardRowItem(
+                modifier = Modifier.align(Alignment.CenterStart),
+                title = "Remaining Allowance",
+                amount = "₹ 21",
+                image = R.drawable.ic_rupee,
+            )
+
+            CardRowItem(
+                modifier = Modifier.align(Alignment.CenterEnd),
+                title = "Monthly Goal",
+                amount = "₹ 5000",
+                image = R.drawable.ic_rupee,
+            )
+        }
+
     }
+}
+
+@Composable
+fun TransactionList(modifier: Modifier){
+    Column (modifier = modifier.padding(16.dp)){
+        Box(modifier = Modifier.fillMaxWidth()){
+            Text( text = "Recent Transaction",
+                fontSize = 20.sp)
+            Text(
+                text = "See All",
+                fontSize = 16.sp,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+
+        }
+        TransactionItem(
+            title = "Shopping",
+            amount = "- $25",
+            icon = R.drawable.ic_notifications,
+            date = "Today",
+            color = Color.Red,
+        )
+
+    }
+
+}
+
+@Composable
+fun CardRowItem(modifier: Modifier, title: String, amount: String, image: Int){
+    Column( modifier = modifier) {
+        Row {
+            Image( painter = painterResource( id = image), contentDescription = null, modifier = Modifier.size(24.dp))
+            Spacer(modifier = Modifier.size(5.dp))
+            Text(
+                text = title,
+                fontSize = 14.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.padding( top = 4.dp)
+            )
+        }
+        Text( text = amount,
+            color = Color.White,
+            fontSize = 20.sp,
+            modifier = Modifier.padding( start = 7.dp)
+
+        )
+    }
+}
+
+@Composable
+fun TransactionItem(title:String, amount: String, icon:Int, date: String, color: Color){
+Box(modifier = Modifier.fillMaxWidth().padding( vertical = 8.dp))
+{
+    Row {
+        Image(
+            painter = painterResource(id = icon), contentDescription = null,
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+
+
+        Column {
+            Text(text = title, fontSize = 16.sp)
+            Text(text = date, fontSize = 16.sp)
+        }
+    }
+    Text( text = amount , fontSize = 20.sp , color = color, modifier = Modifier.align(Alignment.CenterEnd))
+}
+
 }
 
 @Composable
