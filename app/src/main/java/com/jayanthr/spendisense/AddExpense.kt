cbfa968207172ecf59e3c4dcafcedd145dd58838
El Jay <jayanthrahul3@gmail.com>
2024-12-13 03:37:42 +0530

    v0.1.3.1

--- app/src/main/java/com/jayanthr/spendisense/AddExpense.kt
+++ app/src/main/java/com/jayanthr/spendisense/AddExpense.kt
@@ -16,31 +16,45 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.jayanthr.spendisense.data.model.ExpenseEntity
+import com.jayanthr.spendisense.viewmodel.AddExpenseViewModel
+import com.jayanthr.spendisense.viewmodel.AddExpenseViewModelFactory
 import com.jayanthr.spendisense.widget.ExpenseTextView
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun AddExpense(){
+
+    val viewModel = AddExpenseViewModelFactory(LocalContext.current)
+        .create(AddExpenseViewModel::class.java)
+
+    val coroutineScope = rememberCoroutineScope()
     Surface (modifier = Modifier.fillMaxSize()) {
         ConstraintLayout(modifier = Modifier.fillMaxSize()) {
             val (nameRow, list, card, topBar) = createRefs()
@@ -86,13 +100,17 @@ fun AddExpense(){
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
 
-            })
+            }, onAddExpenseClick ={
+                coroutineScope.launch{
+                    viewModel.addExpense(it)
+                }
+            } )
         }
     }
 }
 
 @Composable
-fun DataForm(modifier: Modifier){
+fun DataForm(modifier: Modifier, onAddExpenseClick: (model: ExpenseEntity)-> Unit){
 
     val name = remember {
         mutableStateOf("")
@@ -107,6 +125,13 @@ fun DataForm(modifier: Modifier){
     val dateDialogVisibility = remember {
         mutableStateOf(false)
     }
+    val category = remember {
+        mutableStateOf("")
+    }
+    val type = remember {
+        mutableStateOf("")
+    }
+
 
     Column (modifier = modifier
         .padding(16.dp)
@@ -118,31 +143,57 @@ fun DataForm(modifier: Modifier){
         .verticalScroll(rememberScrollState())
 
     ){
-
+        //NAME
         ExpenseTextView( text = "Name", fontSize = 14.sp)
-        Spacer(modifier = Modifier.size(8.dp))
+        Spacer(modifier = Modifier.size(4.dp))
         OutlinedTextField(value = name.value, onValueChange = {
             name.value = it
         }, modifier = Modifier.fillMaxWidth())
-
-        ExpenseTextView( text = "Amount", fontSize = 14.sp)
         Spacer(modifier = Modifier.size(8.dp))
-        OutlinedTextField(value = name.value, onValueChange = {
+
+        //AMOUNT
+        ExpenseTextView( text = "Amount", fontSize = 14.sp)
+        Spacer(modifier = Modifier.size(4.dp))
+        OutlinedTextField(value = amount.value, onValueChange = {
             amount.value = it
         }, modifier = Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.size(8.dp))
 
         //DATE
         ExpenseTextView( text = "Date", fontSize = 14.sp)
-        Spacer(modifier = Modifier.size(8.dp))
-        OutlinedTextField(value = date.value.toString(),
+        Spacer(modifier = Modifier.size(4.dp))
+        OutlinedTextField(value = if(date.value == 0L)"" else Utils.formatDateToHumanReadableFormat(date.value),
             onValueChange = {}, modifier = Modifier
             .fillMaxWidth()
             .clickable{ dateDialogVisibility.value = true},
             enabled = false)
+        Spacer(modifier = Modifier.size(8.dp))
+
         //DROP DOWN
+        ExpenseTextView( text = "Category", fontSize = 14.sp)
+        Spacer(modifier = Modifier.size(4.dp))
+        ExpenseDropDown(listOf("Food","Shopping", "Travel", "Other"),
+            onItemSelected = {category.value = it})
+        Spacer(modifier = Modifier.size(8.dp))
 
         //TYPE
-        Button(onClick = {},
+        ExpenseTextView( text = "Type", fontSize = 14.sp)
+        Spacer(modifier = Modifier.size(4.dp))
+        ExpenseDropDown(listOf("Income","Expense"),
+            onItemSelected = {type.value = it})
+        Spacer(modifier = Modifier.size(8.dp))
+
+        Button(onClick = {
+            val model = ExpenseEntity(
+                null,
+                name.value,
+                amount.value.toDoubleOrNull() ?: 0.0,
+                Utils.formatDateToHumanReadableFormat(date.value),
+                category.value,
+                type.value
+            )
+            onAddExpenseClick(model)
+        },
             modifier = Modifier
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(2.dp)))
@@ -161,6 +212,7 @@ fun DataForm(modifier: Modifier){
     }
 }
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpenseDatePickerDialog(
@@ -173,16 +225,50 @@ fun ExpenseDatePickerDialog(
         onDismissRequest =  {onDismiss()},
         confirmButton = { TextButton(onClick = {onDateSelected(selectedDate)})
             {
-                Text( text = "Confirm")
+                ExpenseTextView( text = "Confirm")
            }
         },
         dismissButton = {TextButton(onClick = {onDateSelected(selectedDate)})
             {
-                Text( text = "Cancel")
+                ExpenseTextView( text = "Cancel")
             }
         }
     ) {
         DatePicker(state = datePickerState)
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExpenseDropDown(listOfItems: List<String>, onItemSelected:(item: String) -> Unit){
+    val expanded = remember { mutableStateOf(false) }
+    val selectedItem = remember { mutableStateOf(listOfItems[0]) }
+
+
+    ExposedDropdownMenuBox(
+        expanded = expanded.value,
+        onExpandedChange = { expanded.value = it } )
+    {
+        TextField(value = selectedItem.value, onValueChange ={},
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor(),
+                readOnly = true,
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon( expanded = expanded.value)
+            }
+        )
+
+        ExposedDropdownMenu(expanded = expanded.value, onDismissRequest = {}) {
+            listOfItems.forEach{
+                DropdownMenuItem(
+                    text = {ExpenseTextView(text = it)},
+                    onClick = { selectedItem.value = it
+                        onItemSelected(selectedItem.value)
+                    expanded.value = false})
+            }
+        }
     }
 }
 
--- app/src/main/java/com/jayanthr/spendisense/Utils.kt
+++ app/src/main/java/com/jayanthr/spendisense/Utils.kt
@@ -1,4 +1,12 @@
 package com.jayanthr.spendisense
 
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 object Utils {
+
+    fun formatDateToHumanReadableFormat(dateInMillis: Long): String{
+        val dateFormatter = SimpleDateFormat("dd/mm/yyyy", Locale.getDefault())
+        return dateFormatter.format(dateInMillis)
+    }
 }
